28af795726b0eca77de773848d5473528a3d81a0
Andreas <andreas.thuen@spv.no>
2023-08-31 13:40:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# ingress2acmebotreflector
+A simple k8s controller that works with the https://github.com/shibayan/keyvault-acmebot project to move cert orders from inside the cluster to the outside.
+The application works from inside the cluster by watching all ingress resources and every time an ingress resource is changed or created it checks the acmebot api if a 
+tls cert exists for the given ingress, and if not it orders a cert for the domain in question.
+
+The image for the controller can be found on dockerhub: https://hub.docker.com/r/spvest/ingress2acmebotreflector
+
+## Description
+// TODO(user): An in-depth paragraph about your project and overview of use
+
+## Getting Started
+1. Install keyvault acmebot in your azure environment
+2. Set up a the keyvault acmebot the expose an api through the azure portal
+3. Grant the managed-identity of the k8s cluster where the controller is access to the acmebot api, this can be done using the script in the scripts folder.
+4. Install the controller in your cluster by using the sample deployment file in the project.
+
+
+
+### Running on the cluster
+1. Install Instances of Custom Resources:
+
+```sh
+kubectl apply -f config/samples/
+```
+
+2. Build and push your image to the location specified by `IMG`:
+
+```sh
+make docker-build docker-push IMG=<some-registry>/ingress2acmebotreflector:tag
+```
+
+3. Deploy the controller to the cluster with the image specified by `IMG`:
+
+```sh
+make deploy IMG=<some-registry>/ingress2acmebotreflector:tag
+```
+
+### Uninstall CRDs
+To delete the CRDs from the cluster:
+
+```sh
+make uninstall
+```
+
+### Undeploy controller
+UnDeploy the controller from the cluster:
+
+```sh
+make undeploy
+```
+
+## Contributing
+// TODO(user): Add detailed information on how you would like others to contribute to this project
+
+### How it works
+This project aims to follow the Kubernetes [Operator pattern](https://kubernetes.io/docs/concepts/extend-kubernetes/operator/).
+
+It uses [Controllers](https://kubernetes.io/docs/concepts/architecture/controller/),
+which provide a reconcile function responsible for synchronizing resources until the desired state is reached on the cluster.
+
+### Test It Out
+1. Install the CRDs into the cluster:
+
+```sh
+make install
+```
+
+2. Run your controller (this will run in the foreground, so switch to a new terminal if you want to leave it running):
+
+```sh
+make run
+```
+
+**NOTE:** You can also run this in one step by running: `make install run`
+
+### Modifying the API definitions
+If you are editing the API definitions, generate the manifests such as CRs or CRDs using:
+
+```sh
+make manifests
+```
+
+**NOTE:** Run `make --help` for more information on all potential `make` targets
+
+More information can be found via the [Kubebuilder Documentation](https://book.kubebuilder.io/introduction.html)
+
+## License
+
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ controllers/ingress_controller.go
@@ -0,0 +1,221 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	ingress "k8s.io/api/networking/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"net/http"
+	"os"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// IngressReconciler reconciles a Ingress object
+type IngressReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+type KeyVaultInfo struct {
+	ID                string    `json:"id"`
+	Name              string    `json:"name"`
+	DNSNames          []string  `json:"dnsNames"`
+	CreatedOn         time.Time `json:"createdOn"`
+	ExpiresOn         time.Time `json:"expiresOn"`
+	X509Thumbprint    string    `json:"x509Thumbprint"`
+	KeyType           string    `json:"keyType"`
+	KeySize           int       `json:"keySize"`
+	KeyCurveName      *string   `json:"keyCurveName"`
+	ReuseKey          bool      `json:"reuseKey"`
+	IsExpired         bool      `json:"isExpired"`
+	IsIssuedByAcmebot bool      `json:"isIssuedByAcmebot"`
+	IsSameEndpoint    bool      `json:"isSameEndpoint"`
+	AcmeEndpoint      string    `json:"acmeEndpoint"`
+}
+
+type CertOrder struct {
+	DnsNames []string `json:"DnsNames"`
+}
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+var logger = log.Log.WithName("ingress_controller")
+
+var api_scope = os.Getenv("API_SCOPE")
+var backend = os.Getenv("BACKEND")
+var azure_ad_client_id = os.Getenv("AZURE_AD_CLIENT_ID")
+var domains = strings.Split(os.Getenv("DOMAINS"), ",")
+
+var s = flag.String("s", api_scope+"/.default", "scope for access token")
+
+func getJson(url string, target interface{}, token string) error {
+
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+
+	if err != nil {
+		return err
+	}
+	r, err := myClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func checkExistingCerts(dnsName string, keyVaultInfoList []KeyVaultInfo) bool {
+	logger.Info("Found " + strconv.Itoa(len(keyVaultInfoList)) + " certs in the keyvault")
+	for _, keyVaultInfo := range keyVaultInfoList {
+		for _, dns := range keyVaultInfo.DNSNames {
+			if dns == dnsName {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+//+kubebuilder:rbac:groups=core,resources=ingresses/finalizers,verbs=get;list
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+func (r *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger = log.FromContext(ctx)
+	var ing ingress.Ingress
+
+	flag.Parse()
+	opts := &azidentity.ManagedIdentityCredentialOptions{}
+	opts.ID = azidentity.ClientID(azure_ad_client_id)
+
+	cred, err := azidentity.NewManagedIdentityCredential(opts)
+	if err != nil {
+		logger.Error(err, "unable to create managed identity credential")
+		return ctrl.Result{}, nil
+	}
+
+	logger.Info("Calling GetToken()...")
+	tk, err := cred.GetToken(context.Background(), policy.TokenRequestOptions{Scopes: []string{*s}})
+	if err != nil {
+		logger.Error(err, "unable to get token")
+		return ctrl.Result{}, nil
+	}
+
+	logger.Info("Reconciling Ingress")
+	if err := r.Get(ctx, req.NamespacedName, &ing); err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Info("Ingress resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		logger.Error(err, "unable to fetch Ingress")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	if ing.Spec.TLS == nil {
+		logger.Info("No TLS found. Ignoring")
+		return ctrl.Result{}, nil
+	}
+	secretName := ing.Spec.TLS[0].SecretName
+	host := ing.Spec.Rules[0].Host
+	logger.Info("TLS found, secret name: " + secretName + " host: " + host)
+
+	var keyVaultInfoList []KeyVaultInfo
+
+	//check if the current host is in the list of supported domains
+	var validDomain bool
+	for _, domain := range domains {
+		if strings.Contains(host, domain) {
+			validDomain = true
+		}
+	}
+	if !validDomain {
+		logger.Info("Host not in list of supported domains, exiting")
+		return ctrl.Result{}, nil
+	}
+
+	err = getJson(backend+"/api/certificates", &keyVaultInfoList, tk.Token)
+	if err != nil {
+		logger.Error(err, "unable to get cert list")
+		return ctrl.Result{}, err
+	}
+
+	logger.Info("checking existing certs")
+	existingCerts := checkExistingCerts(host, keyVaultInfoList)
+
+	//if no existing certs, order new cert through the rest api
+	if !existingCerts {
+		logger.Info("No cert found in keyvault, initiating order")
+		certorder := CertOrder{DnsNames: []string{host}}
+
+		payloadBuf := new(bytes.Buffer)
+
+		json.NewEncoder(payloadBuf).Encode(certorder)
+
+		req, err := http.NewRequest("POST", backend+"/api/certificate", payloadBuf)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+tk.Token)
+
+		response, err := myClient.Do(req)
+		if err != nil {
+			logger.Error(err, "unable to order new cert")
+			return ctrl.Result{}, err
+		}
+		defer response.Body.Close()
+		println("Status: ", response.Status)
+		certorderUrl := response.Header.Get("Location")
+		logger.Info("Cert ordered, waiting for 30 seconds, callback: " + certorderUrl)
+		time.Sleep(30 * time.Second)
+		logger.Info("Checking if cert is ready")
+		req2, err := http.NewRequest("GET", certorderUrl, nil)
+		resp, err := myClient.Do(req2)
+		req2.Header.Set("Authorization", "Bearer "+tk.Token)
+
+		if err != nil {
+			logger.Error(err, "unable to get cert status")
+			return ctrl.Result{}, err
+		} else if resp.StatusCode == 200 {
+			logger.Info("Cert is ready")
+		}
+		defer resp.Body.Close()
+	} else {
+		logger.Info("Cert found in keyvault, exiting")
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *IngressReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&ingress.Ingress{}).
+		Complete(r)
+}
